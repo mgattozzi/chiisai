3dc0636bc80c86fdb9fdebac2780d30e5ff4a8cf
Michael Gattozzi <mgattozzi@gmail.com>
2017-11-02 11:37:58 -0400

    Add ability to have url parameters in routes
    This commit adds an example as well code to handle url parameters in
    your routes. Bumps the version up to 0.1.1 for use elsewhere when
    published.

---
+++ examples/url_parameters.rs
@@ -0,0 +1,32 @@
+#[macro_use] extern crate chiisai;
+
+// Imports traits and the rexported hyper and futures crates
+use chiisai::*;
+use futures::future::ok;
+
+
+fn main() {
+    let server = Chiisai::new()
+                            // We define routes here with parameters here.
+                            // If the section of the url you want to be a
+                            // parameter starts with : code inside chiisai
+                            // will handle it properly to match.
+                         .routes(router! {
+                            ("/test/:param/test2", Parameter)
+                            ("/test/:param/test2", PostParameter)
+                            ("/test/:user/test2", PostParameter)
+                         });
+    server.run().unwrap();
+}
+
+
+routes!(
+    (Get, Parameter, |req: Request| {
+        println!("{:?}", req);
+        ok(Response::new())
+    })
+    (Post, PostParameter, |req: Request| {
+        println!("{:?}", req);
+        ok(Response::new())
+    })
+);
